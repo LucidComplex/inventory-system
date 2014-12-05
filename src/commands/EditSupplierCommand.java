614aef0be441fc7ab7932ddcc121c49c83fc9754
MiriamMarie <MiriamMarie@192.168.2.105>
2014-12-05 18:56:38 +0800

    Edit Supplier Command

--- src/commands/EditSupplierCommand.java
+++ src/commands/EditSupplierCommand.java
@@ -8,6 +8,9 @@ package commands;
 import base.Command;
 import base.UI;
 import exceptions.ExecutorException;
+import javax.swing.*;
+import models.Address;
+import models.Supplier;
 
 /**
  *
@@ -22,7 +25,39 @@ public class EditSupplierCommand extends Command {
     
     @Override
     public void execute() throws ExecutorException {
+        JComboBox editSupplier = (JComboBox) fields.get("supplier_combo");
+        Supplier supplier = (Supplier) editSupplier.getSelectedItem();
+        Address address = supplier.getAddress();
         
+        JRadioButton name = (JRadioButton) fields.get("name_radiobutton");
+        JRadioButton street = (JRadioButton) fields.get("street_radiobutton");
+        JRadioButton city = (JRadioButton) fields.get("city_radiobutton");
+        JRadioButton country = (JRadioButton) fields.get("country_radiobutton");
+        JRadioButton province = (JRadioButton) fields.get("province_radiobutton");
+        JRadioButton zipcode = (JRadioButton) fields.get("zipcode_radiobutton");
+        
+        JTextField newName = (JTextField) fields.get("name_field");
+        JTextField newStreet = (JTextField) fields.get("street_field");
+        JTextField newCity = (JTextField) fields.get("city_field");
+        JTextField newCountry = (JTextField) fields.get("country_field");
+        JTextField newProvince = (JTextField) fields.get("province_field");
+        JTextField newZipcode = (JTextField) fields.get("zipcode_field");
+        
+        if(name.isSelected())
+            supplier.setName(newName.getText());
+        if(street.isSelected())
+            address.setStreet(newStreet.getText());
+        if(city.isSelected())
+            address.setCity(newCity.getText());
+        if(country.isSelected())
+            address.setCountry(newCountry.getText());
+        if(province.isSelected())
+            address.setProvince(newProvince.getText());
+        if(zipcode.isSelected())
+            address.setZipcode(newZipcode.getText());
+            
+//        supplier.setAddress(address);
+        supplier.update();
     }
     
 }
